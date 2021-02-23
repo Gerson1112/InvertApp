1ef67b3b0aa29181ce547c984a517e54ad00db85
Gerson Gil <20198785@itla.edu.do>
2021-02-22 20:48:55 -0500

    HOLA

---
+++ InvertApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InvertApp
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            iMenu Principal = new Menuprincipal();
+            Principal.imprimirmenu(); 
+
+        }
+    }
+}
---
+++ InvertApp/Salida de invetario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InvertApp
+{
+    public class Salida_de_invetario
+    {
+        public void Listasalida()
+        {
+            for (int i = 0; i < repositorio.Instancia.Listarproductos.Count; i++)
+            {
+                producto prodcutointerada = repositorio.Instancia.Listarproductos[i];
+                Console.WriteLine((i + 1 + "- " + " Nombre: " + prodcutointerada.Nombre + " Cantidad: " + prodcutointerada.Cantidad));
+            }
+            
+        }
+        public void salidadeinventario()
+        {
+            Listasalida();
+            Console.WriteLine("Selecciona un producto: ");
+            int producto = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la cantidad que desea sacar del inventario: ");
+            int salida = int.Parse(Console.ReadLine());
+
+            int inventario = repositorio.Instancia.Listarproductos[producto - 1].Cantidad - salida;
+            repositorio.Instancia.Listarproductos[producto - 1].Cantidad = inventario;
+
+            Console.WriteLine("Se ha salido la cantidad introducida del producto");
+            Console.ReadKey();
+        }
+    }
+}
